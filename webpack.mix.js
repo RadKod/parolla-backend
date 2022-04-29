1414378afb76a14478d47faf583bb4d9d232c83b
Eray Aydın <erayaydinn@protonmail.com>
2022-04-29 18:52:35 +0300

    :hammer: Fix webpack build and 3rd party deps.
    - Adds `copyDirectory` directive to the webpack configuration for
      copying image assets.
    - Adds 3rd party :package: dependencies to the `package.json` file.
    - Remove unnecessary images from the public directory.
    - Remove unnecessary static vendor files.

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,5 +13,6 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
+    .copyDirectory('resources/images', 'public/img')
     .sourceMaps();
 
