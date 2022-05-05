22355179517f7394b97fc05f5e3eb8b4820dbc3b
Eray Aydın <erayaydinn@protonmail.com>
2022-05-05 08:34:25 +0300

    :coffin: Remove unnecessary providers.
    - Removes boilerplate `AuthServiceProvider`, `BroadcastServiceProvider`,
      `EventServiceProvider` providers from the project. Fixes #8 issue.
    - Moves `FortifyServiceProvider` and `JetstreamServiceProvider` logics
      to the `AppServiceProvider` for simplicity. Removes unused providers
      from the project. It's also help for fix #8 issue.
    - :recycle: Remove unnecessary `channels.php` route file from the project.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,18 @@
 
 namespace App\Providers;
 
+use App\Actions\Jetstream\DeleteUser;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
+use App\Actions\Fortify\CreateNewUser;
+use App\Actions\Fortify\ResetUserPassword;
+use App\Actions\Fortify\UpdateUserPassword;
+use App\Actions\Fortify\UpdateUserProfileInformation;
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\RateLimiter;
+use Laravel\Fortify\Fortify;
+use Laravel\Jetstream\Jetstream;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,6 +34,60 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        $this->bootSchema();
+        $this->setupFortify();
+        $this->setupJetstream();
+    }
+
+    /**
+     * Boots up the Schema and migration related stuff.
+     *
+     * @return void
+     */
+    protected function bootSchema()
+    {
         Schema::defaultstringLength(191);
+    }
+
+    /**
+     * Setup fortify package for authentication.
+     *
+     * @return void
+     */
+    protected function setupFortify()
+    {
+        Fortify::createUsersUsing(CreateNewUser::class);
+        Fortify::updateUserProfileInformationUsing(UpdateUserProfileInformation::class);
+        Fortify::updateUserPasswordsUsing(UpdateUserPassword::class);
+        Fortify::resetUserPasswordsUsing(ResetUserPassword::class);
+
+        RateLimiter::for('login', function (Request $request) {
+            $email = (string) $request->email;
+
+            return Limit::perMinute(5)->by($email.$request->ip());
+        });
+
+        RateLimiter::for('two-factor', function (Request $request) {
+            return Limit::perMinute(5)->by($request->session()->get('login.id'));
+        });
+    }
+
+    /**
+     * Setup jetstream package
+     *
+     * @return void
+     */
+    protected function setupJetstream()
+    {
+        Jetstream::defaultApiTokenPermissions(['read']);
+
+        Jetstream::permissions([
+            'create',
+            'read',
+            'update',
+            'delete',
+        ]);
+
+        Jetstream::deleteUsersUsing(DeleteUser::class);
     }
 }
--- config/app.php
+++ config/app.php
@@ -189,12 +189,7 @@ return [
          * Application Service Providers...
          */
         App\Providers\AppServiceProvider::class,
-        App\Providers\AuthServiceProvider::class,
-        // App\Providers\BroadcastServiceProvider::class,
-        App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        App\Providers\FortifyServiceProvider::class,
-        App\Providers\JetstreamServiceProvider::class,
 
     ],
 
