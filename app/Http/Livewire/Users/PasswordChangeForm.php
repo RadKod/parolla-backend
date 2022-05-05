a3c4e6ca24f980c1731d7381efe2ee7f3688c4e8
Eray Aydın <erayaydinn@protonmail.com>
2022-05-05 14:08:16 +0300

    :recycle: Refactors Users livewire components.

--- app/Http/Livewire/Users/PasswordChangeForm.php
+++ app/Http/Livewire/Users/PasswordChangeForm.php
@@ -3,27 +3,45 @@
 namespace App\Http\Livewire\Users;
 
 use App\Actions\Fortify\UpdateUserPassword;
+use Illuminate\Validation\ValidationException;
+use Illuminate\View\View;
 use Livewire\Component;
 
 class PasswordChangeForm extends Component
 {
-    public $state = [
+    /**
+     * @var array|string[] current state of the form
+     */
+    public array $state = [
         'current_password' => '',
         'password' => '',
         'password_confirmation' => '',
     ];
 
-    public function render()
+    /**
+     * Renders component.
+     *
+     * @return View
+     */
+    public function render(): View
     {
-        return view('livewire.users.password-change-form');
+        return view('users.password-change-form');
     }
 
+    /**
+     * Updates profile information with associated user.
+     *
+     * @param UpdateUserPassword $updater
+     * @return void
+     * @throws ValidationException
+     */
     public function updateProfileInformation(UpdateUserPassword $updater): void
     {
         $this->resetErrorBag();
 
         $updater->update(auth()->user(), $this->state);
 
+        // Resets current state
         $this->state = [
             'current_password' => '',
             'password' => '',
