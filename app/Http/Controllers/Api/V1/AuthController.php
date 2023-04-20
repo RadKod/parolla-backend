80a1fd72a0baa93c67df6035bacb93172e866083
Abdullah <abdullah@radkod.com>
2023-04-20 17:45:46 +0300

    :art: auth feature added

---
+++ app/Http/Controllers/Api/V1/AuthController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Models\User;
+use Exception;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends BaseController
+{
+    /**
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function me(Request $request): JsonResponse
+    {
+        $validator = Validator::make($request->all(), [
+            'fingerprint' => 'required|string',
+        ]);
+        if ($validator->fails()) {
+            return $this->sendError(
+                "Validation Error",
+                $validator->errors()->toArray()
+            );
+        }
+
+        $user = User::query()
+            ->where('fingerprint', $request->get('fingerprint'))
+            ->first();
+        if (!$user) {
+            return $this->sendError(
+                'User not found'
+            );
+        }
+
+        return $this->sendResponse(
+            $user,
+            'User found'
+        );
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function update(Request $request): JsonResponse
+    {
+        $fingerprint = $request->get('fingerprint');
+        $username = $request->get('username');
+        $validator = Validator::make($request->all(), [
+            'username' => 'string|unique:users,username'.($fingerprint ? ','.$fingerprint.',fingerprint' : ''),
+            'fingerprint' => 'required|string',
+        ]);
+        if ($validator->fails()) {
+            return $this->sendError(
+                "Validation Error",
+                $validator->errors()->toArray()
+            );
+        }
+
+        $user = User::query()
+            ->where('fingerprint', $fingerprint)
+            ->first();
+        if (!$user) {
+            $user = User::query()->create([
+                'username' => $username,
+                'fingerprint' => $fingerprint,
+                'password' => bcrypt($fingerprint) . random_int(1000, 9999),
+                'email' => $fingerprint . '@example.com',
+            ]);
+        }
+
+        $user->update([
+            'username' => $username,
+        ]);
+
+        return $this->sendResponse(
+            $user,
+            'User updated'
+        );
+    }
+}
